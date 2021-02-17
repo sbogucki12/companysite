1712c88485a5293b661ca76a77d74d82a95ea532
Steve B <sbogucki@mail.usf.edu>
2021-02-17 08:50:38 -0800

    added onresize listener

--- src/App.js
+++ src/App.js
@@ -1,99 +1,27 @@
 import './App.css';
 import { useState } from 'react';
-import DialogMain from './DialogMain';
-import Landing from './Landing';
+import DesktopBase from './desktop/DesktopBase';
 
 function App() {
-	const [dialog, setDialog] = useState('landing');
-	const [showLanding, setShowLanding] = useState(true);
+	const [viewportWidth, setViewportWidth] = useState(window.innerWidth);
 
-	function onClickSetDialog(e, component) {
-		e.stopPropagation();
-		setDialog(component);
-	}
+	let view = <DesktopBase />;
 
-	function clickAwaySetDialog(e) {
-		e.stopPropagation();
-		setDialog('main');
-		console.log('main');
-	}
-
-	const Btn = (props) => {
-		return (
-			<div>
-				<button style={{ whiteSpace: 'nowrap' }} onClick={(e) => onClickSetDialog(e, props.component)}>
-					{props.label}
-				</button>
-			</div>
-		);
+	window.onresize = function () {
+		setViewportWidth(window.innerWidth);
 	};
 
-	function ButtonsMain() {
-		return (
+	if (viewportWidth < 450) {
+		view = (
 			<div>
-				<div id="about">
-					<Btn component={'about'} label={'Show About'} />
-				</div>
-				<div id="experience">
-					<Btn component={'experience'} label={'Show Experience'} />
-				</div>
-				<div id="values">
-					<Btn component={'values'} label={'Show Values'} />
-				</div>
-				<div id="contact">
-					<Btn component={'contact'} label={'Show Contact'} />
-				</div>
+				<h1>Mobile!</h1>
 			</div>
 		);
+	} else if (viewportWidth >= 450) {
+		view = <DesktopBase />;
 	}
 
-	let dialogView = null;
-
-	switch (dialog) {
-		case 'about':
-			dialogView = <DialogMain label={'about'} clickAwaySetDialog={clickAwaySetDialog} />;
-			break;
-		case 'experience':
-			dialogView = <DialogMain label={'experience'} clickAwaySetDialog={clickAwaySetDialog} />;
-			break;
-		case 'values':
-			dialogView = <DialogMain label={'values'} clickAwaySetDialog={clickAwaySetDialog} />;
-			break;
-		case 'contact':
-			dialogView = <DialogMain label={'contact'} clickAwaySetDialog={clickAwaySetDialog} />;
-			break;
-		case 'main':
-			dialogView = null;
-			break;
-		case 'landing':
-			dialogView = null;
-			//<Landing setDialog={setDialog} />;
-			break;
-		default:
-			dialogView = null;
-	}
-
-	let main = <Landing setDialog={setDialog} setShowLanding={setShowLanding}/>
-
-	if(!showLanding){
-		main = (
-			<div id="main" onClick={(e) => onClickSetDialog(e, 'main')} style={{ minHeight: '100vh' }}>
-				<div id="mainBox">
-					<ButtonsMain />
-					{dialogView}
-				</div>
-				<div id="homeButtonContainer">
-					<button onClick={() => setShowLanding(true)}>
-						Home
-					</button>
-				</div>
-			</div>
-		);
-	}
-
-	let view = main;
-
-	return <div className="App">{view}</div>;
+	return <div>{view}</div>;
 }
 
 export default App;
